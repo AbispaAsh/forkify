38b26874d944015cbf67109110df2aa9ad747c72
AbispaAsh <palaash.sarkar@gmail.com>
2021-11-11 20:59:34 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const API_KEY = `3bd25d17-8fb9-4415-8519-502d4cadd8fb`;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
